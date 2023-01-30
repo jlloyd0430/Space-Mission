f23a5bb07facb6eef148afa81fbb76232cb8cec4
jesse Lloyd <jesselloyd911@gmail.com>
2023-01-30 11:02:02 -0500

    refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Gallery from "./Gallery";
+import Contact from "./Contact";
+import Mission from "./Mission";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        {<Header />}
+        {<Navbar />}
+      </div>
+      <div className="row">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
   );
 }
